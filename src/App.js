8a0b8cc56cd095121ebfbcce3ec4fe288fdb783f
Hunter White <hunterwhite.dev@gmail.com>
2020-09-23 09:52:21 -0400

    Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
+  <Router>
     <div className="App">
-     <h1>Lets build an amazon clone!!!!</h1>
+    <Switch>
+    <Route path="/checkout">
+       <h1>Checkout!!</h1>
+     </Route>
+     <Route path="/login">
+       <h1>Login Page!!</h1>
+     </Route>
+     <Route path="/">
+       <h1>Home Page!!</h1>
+     </Route>
+    </Switch>
     </div>
+  </Router>
+
+
   );
 }
 
